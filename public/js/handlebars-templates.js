571020581e785dc9f2540cb75a8772158868d867
Simon L <slarcher86@gmail.com>
2015-08-05 03:21:49 +0200

    Basic index page for skeleton

--- public/js/handlebars-templates.js
+++ public/js/handlebars-templates.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['demo'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<div data-alert class=\"alert-box success radius\">\n  Successfully connected to server.\n  <a href=\"#\" class=\"close\">&times;</a>\n</div>\n";
+    return "<div data-alert class=\"alert-box success radius\">\n  Successfully connected to socket.io server.\n  <a href=\"#\" class=\"close\">&times;</a>\n</div>\n";
 },"useData":true});
 })();
